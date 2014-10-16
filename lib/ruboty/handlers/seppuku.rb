25661bc16f5dda415bedca9cfee4f53211626a1d
block_given? <block_given@outlook.com>
2014-10-17 01:32:03 +0900

    seppuku dyno

--- lib/ruboty/handlers/seppuku.rb
+++ lib/ruboty/handlers/seppuku.rb
@@ -1,6 +1,9 @@
 module Ruboty
   module Handlers
     class Seppuku < Base
+      env :HEROKU_API_KEY,  'heroku api key',  optional: true
+      env :HEROKU_APP_NAME, 'heroku app name', optional: true
+
       on /(seppuku|harakiri|切腹|腹切り)/i, name: 'seppuku', description: 'seppuku'
 
       def seppuku(message)
--- lib/ruboty/seppuku/actions/seppuku.rb
+++ lib/ruboty/seppuku/actions/seppuku.rb
@@ -3,7 +3,29 @@ module Ruboty
     module Actions
       class Seppuku < Ruboty::Actions::Base
         def call
+          seppuku_dyno if heroku?
           Kernel.exit
+        end
+
+        private
+
+        def seppuku_dyno
+          require 'platform-api'
+          heroku  = PlatformAPI.connect(heroku_api_key)
+          seppuku = {"process" => "bot", "quantity" => 0}
+          heroku.formation.batch_update(heroku_app_name, {"updates" => [seppuku]})
+        end
+
+        def heroku?
+          heroku_api_key and heroku_app_name
+        end
+
+        def heroku_app_name
+          ENV['HEROKU_APP_NAME']
+        end
+
+        def heroku_api_key
+          ENV['HEROKU_API_KEY']
         end
       end
     end
